d62550089483cad04219c7ab1fb9b3d166eeef64
John-Bruce Noad <johnnoad04@gmail.com>
2023-01-11 21:33:25 +0200

    Projects and Resume Responsiveness

--- src/views/ProjectsView.vue
+++ src/views/ProjectsView.vue
@@ -100,4 +100,118 @@ export default {
 .project-btn:active {
   scale: 0.9;
 }
+
+h1, h2, h3, h4, h5, h6, p{
+  word-wrap: break-word;
+}
+
+@media only screen and (max-width: 720px) {
+  #home .profile-img {
+    width: 300px;
+    height: 300px;
+  }
+
+  #about {
+    padding-bottom: 40px;
+  }
+  .about-img {
+    grid-column: 1/-1;
+    margin: 10px auto;
+  }
+  .about-text {
+    grid-column: 1/-1;
+    margin-top: 20px;
+  }
+
+  .education-block p {
+    grid-column: 1/-1;
+  }
+  .education-block h5, .education-block h6 {
+    grid-column: 1/-1;
+  }
+  .education-block strong {
+    color: #fff !important;
+  };
+
+  .skills-block {
+    height: 550px;
+    margin-bottom: 40px;
+  }
+  .html-skill {
+    grid-column: 1/-1;
+  }
+  .css-skill {
+    grid-column: 1/-1;
+  }
+  .javascript-skill {
+    grid-column: 1/-1;
+  }
+  .bootstrap-skill {
+    grid-column: 1/-1;
+  }
+  .excel-skill {
+    grid-column: 1/-1;
+  }
+  .soft-skills {
+    grid-column: 1/-1;
+  }
+}
+
+@media only screen and (max-width: 400px) {
+  #about img, #home img {
+    margin: auto;
+    width: 85%;
+  }
+  #home .profile-img {
+    width: 250px;
+    height: 250px;
+  }
+  #about .about-container {
+    display: flex;
+    flex-direction: column;
+  }
+  .about-text .cv-btns {
+    margin-bottom: 10px;
+    font-size: 15px;
+  }
+  .skills-block {
+    width: 90%;
+  }
+  .project-card {
+    width: 90%;
+  }
+  h2 {
+    font-size: 2.5rem;
+  }
+}
+
+@media only screen and (max-width: 300px) {
+  h2 {
+    font-size: 2rem;
+  }
+  #home .profile-img {
+    width: 200px;
+    height:  200px;
+  }
+  .footer-items {
+    flex-direction: column;
+  }
+}
+
+@media only screen and (max-width: 200px) {
+  #home .profile-img {
+    width: 150px;
+    height: 150px;
+  }
+  .logo-link {
+    display: none;
+  }
+  .skills-block {
+    width: 95%;
+  }
+  h2 {
+    font-size: 1.5rem;
+  }
+}
+
 </style>
