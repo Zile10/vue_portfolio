4e75722b8bf436e20dea48284e68e7c0f226710f
John-Bruce Noad <johnnoad04@gmail.com>
2023-01-12 21:35:26 +0200

    Updated ToDo List

--- src/components/NavBarVue.vue
+++ src/components/NavBarVue.vue
@@ -2,8 +2,8 @@
   <nav class="text-dark">
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link> |
-    <router-link to="/projects">Projects</router-link> |
     <router-link to="/resume">Resume</router-link> |
+    <router-link to="/projects">Projects</router-link> |
     <router-link to="/testimonials">Testimonials</router-link> |
     <router-link to="/contact">Contact</router-link>
   </nav>
