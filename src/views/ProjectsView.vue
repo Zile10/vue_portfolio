7e1dd2d73118605c0e042f61d4452123941d1bc6
John-Bruce Noad <johnnoad04@gmail.com>
2023-01-14 20:19:59 +0200

    Projects w/ Array

--- src/views/ProjectsView.vue
+++ src/views/ProjectsView.vue
@@ -4,68 +4,75 @@
       <h2 data-aos="zoom-in">Projects</h2>
       
       <Carousel slides="6">
+
+        <!-- Unfortunately, Because Of My Animation Components Used Below, "v-for" 
+              CAN NOT Be used to loop through the array, as this causes conflict
+              with the way Vue handle the "v-for" loop, causing all the animations 
+              to render at once, even if "v-if" was used. 
+        -->
+
         <div class="carousel-item active">
           <ProjectCard 
-            title="Running Man Game!" 
-            siteUrl="https://running-man-game.netlify.app/" 
-            githubRepo="https://github.com/Zile10/runningManGame.git" 
-            imgSrc="https://i.postimg.cc/hGFH4VWQ/running-Man-Game-preview.png" 
-            description="To test my self-taught coding skills, one of my first practise projects was this 'Running Game'. Based on and inspired by Google Chrome's very own Dinosaur Game! Now with an incomplete menu! 😂"
+            :title="projects[0].title" 
+            :siteUrl="projects[0].siteUrl" 
+            :githubRepo="projects[0].githubRepo" 
+            :imgSrc="projects[0].imgSrc" 
+            :description="projects[0].description"
           />
           <StarsAnimation/>
         </div>
   
         <div class="carousel-item">
           <ProjectCard 
-            title="Canvas JS Bouncy Balls Simulation!" 
-            siteUrl="https://canvas-bouncy-balls.netlify.app/" 
-            githubRepo="https://github.com/Zile10/canvasBoucyBalls.git" 
-            imgSrc="https://i.postimg.cc/tTd1vdy3/canvas-Boucy-Balls-preview.png" 
-            description="After learning about canvas.js and all it could do, I began practising using it, by building a few basic simulations. This gravity simulation, was one of those first few simulation. Tip: Click anywhere to restart the simulation."
+            :title="projects[1].title" 
+            :siteUrl="projects[1].siteUrl" 
+            :githubRepo="projects[1].githubRepo" 
+            :imgSrc="projects[1].imgSrc" 
+            :description="projects[1].description"
           />
           <BallsAnimation/>
         </div>
   
         <div class="carousel-item">
           <ProjectCard 
-            title="Life Choices HTML/CSS Revision Project" 
-            siteUrl="https://revision-dummy.netlify.app/" 
-            githubRepo="https://github.com/Zile10/revision_day.git" 
-            imgSrc="https://i.postimg.cc/FKD8y8k3/revision-site-preview.png" 
-            description="A revision project I created during the Life Choices coding course, incorporating various HTML and CSS elements. This includes the use of flexbox, grid, media queries and various other elements used when creating a responsive webpage."
+            :title="projects[2].title" 
+            :siteUrl="projects[2].siteUrl" 
+            :githubRepo="projects[2].githubRepo" 
+            :imgSrc="projects[2].imgSrc" 
+            :description="projects[2].description"
           />
           <DogAnimation/>
         </div>
   
         <div class="carousel-item">
           <ProjectCard 
-            title="JS Calculator" 
-            siteUrl="https://john-bruce-js-calculator.netlify.app/" 
-            githubRepo="https://github.com/Zile10/calculator.git" 
-            imgSrc="https://i.postimg.cc/Y9Cr8vg7/calculator-preview.png" 
-            description="A calculator project I created during the LC Coding Course using JS. The display uses basic HTML and CSS. It can preform basic functions such ad addition subtraction multiplication and division, as well as slightly more complete functions suc as exponents and square roots, all while following the 'BODMAS' rule."
+            :title="projects[3].title" 
+            :siteUrl="projects[3].siteUrl" 
+            :githubRepo="projects[3].githubRepo" 
+            :imgSrc="projects[3].imgSrc" 
+            :description="projects[3].description"
           />
           <MathAnimation/>
         </div>
   
         <div class="carousel-item">
           <ProjectCard 
-            title="JS E-commerce Site " 
-            siteUrl="https://john-bruce-js-eomp.netlify.app/" 
-            githubRepo="https://github.com/Zile10/POS_system.git" 
-            imgSrc="https://i.postimg.cc/t4q7B3f1/Ecom-preview.png" 
-            description="An e-commerce site that I created during the LC Coding Course. With a complete and working products page, checkout system, and admin page, using the CRUD system! Data is stored in and fetched from the local storage."
+            :title="projects[4].title" 
+            :siteUrl="projects[4].siteUrl" 
+            :githubRepo="projects[4].githubRepo" 
+            :imgSrc="projects[4].imgSrc" 
+            :description="projects[4].description"
           />
           <MoneyAnimation/>
         </div>
   
         <div class="carousel-item">
           <ProjectCard 
-            title="HTML & CSS Portfolio" 
-            siteUrl="https://john-bruce-portfolio.netlify.app/" 
-            githubRepo="https://github.com/Zile10/portfolio.git" 
-            imgSrc="https://i.postimg.cc/J4WskV55/portfolio-preview.png" 
-            description="My original portfolio, created in the LC Coding course. This portfolio makes use of only HTML, CSS and a few related libraries, such as bootstrap and Animate On Scroll. I am quite proud of this design, and managed to score highly on this project! 😄"
+            :title="projects[5].title" 
+            :siteUrl="projects[5].siteUrl" 
+            :githubRepo="projects[5].githubRepo" 
+            :imgSrc="projects[5].imgSrc" 
+            :description="projects[5].description"
           />
           <PortfolioAnimation/>
         </div>
@@ -101,7 +108,7 @@ export default {
       projects: [
         {
           project: 'Running Man Game!',
-          sitUrl: "https://running-man-game.netlify.app/",
+          siteUrl: "https://running-man-game.netlify.app/",
           githubRepo:"https://github.com/Zile10/runningManGame.git",
           imgSrc:"https://i.postimg.cc/hGFH4VWQ/running-Man-Game-preview.png",
           description:"To test my self-taught coding skills, one of my first practise projects was this 'Running Game'. Based on and inspired by Google Chrome's very own Dinosaur Game! Now with an incomplete menu! 😂",
@@ -109,7 +116,7 @@ export default {
         },
         {
           project: "Canvas JS Bouncy Balls Simulation!",
-          sitUrl: "https://canvas-bouncy-balls.netlify.app/",
+          siteUrl: "https://canvas-bouncy-balls.netlify.app/",
           githubRepo: "https://github.com/Zile10/canvasBoucyBalls.git",
           imgSrc: "https://i.postimg.cc/tTd1vdy3/canvas-Boucy-Balls-preview.png",
           description: "After learning about canvas.js and all it could do, I began practising using it, by building a few basic simulations. This gravity simulation, was one of those first few simulation. Tip: Click anywhere to restart the simulation.",
@@ -117,7 +124,7 @@ export default {
         },
         {
           project: "Life Choices HTML/CSS Revision Project",
-          sitUrl: "https://revision-dummy.netlify.app/",
+          siteUrl: "https://revision-dummy.netlify.app/",
           githubRepo: "https://github.com/Zile10/revision_day.git",
           imgSrc: "https://i.postimg.cc/FKD8y8k3/revision-site-preview.png",
           description: "A revision project I created during the Life Choices coding course, incorporating various HTML and CSS elements. This includes the use of flexbox, grid, media queries and various other elements used when creating a responsive webpage.",
@@ -125,7 +132,7 @@ export default {
         },
         {
           project: "JS Calculator",
-          sitUrl: "https://john-bruce-js-calculator.netlify.app/",
+          siteUrl: "https://john-bruce-js-calculator.netlify.app/",
           githubRepo: "https://github.com/Zile10/calculator.git",
           imgSrc: "https://i.postimg.cc/Y9Cr8vg7/calculator-preview.png",
           description: "A calculator project I created during the LC Coding Course using JS. The display uses basic HTML and CSS. It can preform basic functions such ad addition subtraction multiplication and division, as well as slightly more complete functions suc as exponents and square roots, all while following the 'BODMAS' rule.",
@@ -133,19 +140,19 @@ export default {
         },
         {
           project: "JS E-commerce Site",
-          sitUrl: "https://john-bruce-js-eomp.netlify.app/",
+          siteUrl: "https://john-bruce-js-eomp.netlify.app/",
           githubRepo: "https://github.com/Zile10/POS_system.git",
           imgSrc: "https://i.postimg.cc/t4q7B3f1/Ecom-preview.png",
           description: "An e-commerce site that I created during the LC Coding Course. With a complete and working products page, checkout system, and admin page, using the CRUD system! Data is stored in and fetched from the local storage.",
-          animation: ""
+          animation: "MoneyAnimation"
         },
         {
           project: "HTML & CSS Portfolio",
-          sitUrl: "https://john-bruce-portfolio.netlify.app/",
+          siteUrl: "https://john-bruce-portfolio.netlify.app/",
           githubRepo: "https://github.com/Zile10/portfolio.git",
           imgSrc: "https://i.postimg.cc/J4WskV55/portfolio-preview.png",
           description: "My original portfolio, created in the LC Coding course. This portfolio makes use of only HTML, CSS and a few related libraries, such as bootstrap and Animate On Scroll. I am quite proud of this design, and managed to score highly on this project! 😄",
-          animation: ""
+          animation: "PortfolioAnimation"
         },
       ]
     }
